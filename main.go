c6fec97c7a4848cfed6c204bcc3675a9aaf75578
TWI <kuipers.izaak@2college.nl>
2021-11-02 17:27:00 +0100

    Improve comments

--- main.go
+++ main.go
@@ -6,43 +6,20 @@ import (
 	"os"
 
 	"github.com/ttacon/chalk"
-	"golang.org/x/term"
 )
 
 func main() {
-	clrScrn()
-
-	w, h := getTerminalSize()
-	out := fmt.Sprint(w) + "x" + fmt.Sprint(h)
-
-	drawText(w-len(out), 0, out, chalk.White.NewStyle().WithBackground(chalk.Black))
-
-	drawRectangle(0, 0, w, 1, chalk.White.NewStyle().WithBackground(chalk.Blue))
-	for i := 2; i < h+1; i++ {
-		drawRectangle(0, i, w, 1, chalk.White.NewStyle().WithBackground(chalk.Yellow))
-	}
-
-	drawDialog("error", "The system cannot find the file specified", 30, 6)
-	drawDialog("aboobooaboobooabooboo ", "hello world", 20, 15)
-
-	fmt.Print("")
-	fmt.Printf("\033[0;0H\033[2A")
-
-	bufio.NewReader(os.Stdin).ReadBytes('\n')
-
-	main()
+	clrScrn()                                                               // Clear the screen
+	w, _ := getTerminalSize()                                               // Get the terminal width (w) and height (h)
+	fillEntireScreen(whiteYellowBg)                                         // Make the entire screen Yellow
+	drawRectangle(0, 0, w, 1, whiteBlueBg)                                  // Draw the headerbar of the screen
+	drawDialog("error", "The system cannot find the file specified", 30, 6) // Draw example dialog one
+	drawDialog("aboobooaboobooabooboo ", "hello world", 20, 15)             // Draw example dialog two
+	fmt.Printf("\033[0;0f")                                                 // Set the cursor position to the top of the screen
+	bufio.NewReader(os.Stdin).ReadBytes('\n')                               // Wait for enter to be pressed
+	clrScrn()                                                               // Clear the screen before ending the program
 }
 
 func clrScrn() {
 	fmt.Printf("%s\033[H\033[2J", chalk.Reset)
-}
-
-func getTerminalSize() (w int, h int) {
-	width, height, err := term.GetSize(0)
-
-	if err == nil {
-		return width, height
-	}
-
-	return 0, 0
 }
--- ui.go
+++ ui.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/ttacon/chalk"
+	"golang.org/x/term"
 )
 
 func drawRectangle(x int, y int, w int, h int, color chalk.Style) {
@@ -32,20 +33,11 @@ func drawRectangle(x int, y int, w int, h int, color chalk.Style) {
 }
 
 func drawText(x int, y int, text string, color chalk.Style) {
-	// Decrease x by one to correct for alignment issues
-	x -= 1
-
-	// Save current cursor position
-	fmt.Printf("\033[s")
-
-	// Set new cursor position
-	fmt.Printf("\033[%v;%vH", y, x+1)
-
-	// Print Text
-	fmt.Printf("%s%s%s", color, text, chalk.Reset)
-
-	// Restore cursor position
-	fmt.Printf("\033[u")
+	x -= 1                                         // Decrease x by one to correct for alignment issues
+	fmt.Printf("\033[s")                           // Save current cursor position
+	fmt.Printf("\033[%v;%vH", y, x+1)              // Set new cursor position
+	fmt.Printf("%s%s%s", color, text, chalk.Reset) // Print Text
+	fmt.Printf("\033[u")                           // Restore cursor position
 }
 
 func drawDialog(title string, message string, x int, y int) {
@@ -53,30 +45,48 @@ func drawDialog(title string, message string, x int, y int) {
 	width := 0
 	height := 6
 
+	// Set the dialog width to the length of the title
 	width = len(title) + 2
 
+	// If the length of the message is higher, set the dialog width to it instead
 	if len(message)+2 > width {
 		width = len(message) + 2
 	}
 
-	// Draw Titlebar
-	drawRectangle(x, y, width, height, chalk.White.NewStyle().WithBackground(chalk.Blue))
+	drawRectangle(x, y, width, height, whiteBlueBg)      // Draw Titlebar
+	drawRectangle(x+2, y+1, width, height, blackBlackBg) // Draw Shadow
+	drawRectangle(x, y+1, width, height-1, blackWhiteBg) // Draw Body
+	drawRectangle(x+1, y+4, 4, 1, blackBlueBg)           // Draw OK button background
 
-	// Draw Shadow
-	drawRectangle(x+2, y+1, width, height, chalk.Black.NewStyle().WithBackground(chalk.Black))
-
-	// Draw Body
-	drawRectangle(x, y+1, width, height-1, chalk.Black.NewStyle().WithBackground(chalk.White))
-
-	// Draw OK button background
-	drawRectangle(x+1, y+4, 4, 1, chalk.Black.NewStyle().WithBackground(chalk.Blue))
-
-	// Draw OK button text
-	drawText(x+2, y+4, "OK", chalk.White.NewStyle().WithBackground(chalk.Blue))
-
-	// Draw window title
-	drawText(x, y, title, chalk.White.NewStyle().WithBackground(chalk.Blue))
-
-	// Draw window message
-	drawText(x+1, y+2, message, chalk.Black.NewStyle().WithBackground(chalk.White))
+	drawText(x+2, y+4, "OK", whiteBlueBg)     // Draw OK button text
+	drawText(x, y, title, whiteBlueBg)        // Draw window title
+	drawText(x+1, y+2, message, blackWhiteBg) // Draw window message
 }
+
+func fillEntireScreen(color chalk.Style) {
+	w, h := getTerminalSize() // get the terminal width (w) and height (h)
+
+	for i := 0; i < h+1; i++ {
+		// For the entire height of the screen draw a rectangle with the width of the screen
+		drawRectangle(0, i, w, 1, color)
+	}
+}
+
+func getTerminalSize() (w int, h int) {
+	width, height, err := term.GetSize(0) // use the terminal module to get the terminal size
+
+	if err == nil {
+		return width, height // if there is no error getting the terminal size, return the terminal size
+	}
+
+	return 0, 0 // if there was an error getting the terminal size, return a size of 0x0.
+}
+
+var (
+	whiteYellowBg chalk.Style = chalk.White.NewStyle().WithBackground(chalk.Yellow)
+	whiteBlueBg   chalk.Style = chalk.White.NewStyle().WithBackground(chalk.Blue)
+	whiteBlackBg  chalk.Style = chalk.White.NewStyle().WithBackground(chalk.Black)
+	blackWhiteBg  chalk.Style = chalk.Black.NewStyle().WithBackground(chalk.White)
+	blackBlueBg   chalk.Style = chalk.Black.NewStyle().WithBackground(chalk.Blue)
+	blackBlackBg  chalk.Style = chalk.Black.NewStyle().WithBackground(chalk.Black)
+)
